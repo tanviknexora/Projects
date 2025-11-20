cfd9626f18fcc32a6723ef93a600ee8a73f8406b
tanviknexora <tanvik@nexora.live>
2025-11-20 15:07:58 +0400

    Refactor Streamlit app for call campaign analysis

--- client.py
+++ client.py
@@ -37,7 +37,7 @@ def classify_status(gr):
         return 'None'
 
 # Streamlit app
-st.markdown("<h1 style='text-align: center;'>Call Campaign Data Analysis App</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Campaign Analysis</h1>", unsafe_allow_html=True)
 
 # Upload files
 crm_file = st.sidebar.file_uploader("Upload CRM user_exports Excel file", type=["xlsx"])
@@ -167,4 +167,5 @@ if crm_file and dialer_file:
 
 else:
     st.info("Please upload both CRM and Dialer Excel files to proceed.")
+
 
